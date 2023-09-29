7d0994d8c9d49a5fa30289e59e607163e3ab1894
HellHorseman <duke.sheffer@yandex.ru>
2023-09-29 20:13:36 +0400

    add executing \start command

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,18 +3,24 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private static final Pattern MESSAGE_PATTERN = Pattern.compile("([0-9\\.:\\s]{16})(\\s)(.+)");
+
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Autowired
     private TelegramBot telegramBot;
@@ -26,12 +32,40 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-
-        });
-
+        updates.forEach(this::updateReceived);
+        logger.info("Processing update: {}", updates);
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    public void updateReceived(Update update) {
+        if (update.message() == null) {
+            return;
+        }
+
+        String messageText = update.message().text();
+        long chatId = update.message().chat().id();
+
+        switch (messageText) {
+            case "/start":
+                sendMessage(chatId);
+                break;
+            default:
+                logger.info("Unexpected message");
+        }
+
+        Matcher matcher = MESSAGE_PATTERN.matcher(messageText);
+        if (matcher.find()) {
+            matchedMessage(chatId, matcher);
+        } else {
+            sendFormatError(chatId);
+        }
+    }
+
+    private void sendMessage(long chatId) {
+        logger.info("Sending message to user");
+        telegramBot.execute(new SendMessage(chatId, "Вас приветствует бот-напоминалка "
+                + "Готов вкалывать!"));
+    }
 }
+
+
